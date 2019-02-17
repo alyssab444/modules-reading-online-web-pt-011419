a1a4bde090d5cb2d16b104bcff49d8a114c2cb99
Alyssa Brown <alyssa.brown@live.com>
2019-02-17 17:33:19 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
-def Dancer
+class Dancer
   include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
 
